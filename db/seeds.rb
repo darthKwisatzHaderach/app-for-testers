5c68fd7999729f1ca9f8e449e15ab14a10eb83a3
Тонкушин Дмитрий <datonkushin@gmail.com>
2015-03-13 16:46:59 +0500

    Added contacts form

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Contact.create(name: "Достоевский Федор Михайлович", position: "Писатель", phone: "+7 322 222 111 00", email: "dostoyevsky@post.ru", address: "Москва, ул.Ленина, д.1")
+Contact.create(name: "Альберт Эйнштейн", position: "Ученый", phone: "8 192 222 111 00", email: "emc@post.de", address: "Германия, город Ульм")
+Contact.create(name: "Ньютон Исаак", position: "Ученый", phone: "8 105 232 111 00", email: "phys@post.com", address: "Англия, д. Фулсторп")
+Contact.create(name: "Огюстен Луи Коши", position: "Математик", phone: "8 105 232 111 00", email: "koshi@post.com", address: "Франция")
+Contact.create(name: "Михаил Васильевич Ломоносов", position: "Ученый, энцеклопедист, физик", phone: "8 105 232 111 00", email: "lompost.com", address: "Российская Империя, с. Денисовка")
+Contact.create(name: "Алексей Петрович", position: "Механик", phone: "8 105 222 333 00", email: "mech@ds.com", address: "Россия, г. Екатеринбург, ул. Белинского 135-444")
+Contact.create(name: "Смирнов Илья Петрович", position: "Техник", phone: "+7 895 999 333 00", email: "tech@ds.com", address: "Россия, г. Саратов, ул. Алещенкова 5-4")
+Contact.create(name: "Федоров Иван Геннадьевич", position: "Музыкант", phone: "8 222 222 111 333", email: "music@ds.com", address: "Россия, г. Волгоград, ул. Белинского 15-42")
+Contact.create(name: "Иоган Бах", position: "Композитор", phone: "8 789 232 111 00", email: "bach@post.com", address: "Германия")
+Contact.create(name: "А.С.Пушкин", position: "Писатель", phone: "8 456 232 456 67", email: "puh@post.com", address: "Россия, Москва")
+Contact.create(name: "Михаил Юрьевич Ломоносов", position: "Писатель", phone: "8 335 232 222 67", email: "lom@post.com", address: "Россия, Санкт-Петербург")
